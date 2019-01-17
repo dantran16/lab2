0eef28a464e7667c4e249a7b38a8c88878a0fd7d
Dan Tran <ddt003@ucsd.edu>
2019-01-17 08:55:10 -0800

    added a button

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -1,4 +1,7 @@
 function initializePage(){
+	$("#testjs").click(function(e){
+		$('.jumbotron h1').text("Javascript is connected");
+	});
 	$("a.thumbnail").click(projectClick);
 }
 function projectClick(e) {
