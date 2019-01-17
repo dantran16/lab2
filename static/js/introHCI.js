dd70f5d43a99f037bcad5318f2cc820966de4143
Dan Tran <ddt003@ucsd.edu>
2019-01-17 00:04:50 -0800

    added a dummy hyper ref to first image

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -1,9 +1,6 @@
 function initializePage(){
-	$("#testjs").click(function(e) {
-		$('.jumbotron hl').text("Javascript is connected");
-	});
 	$("a.thumbnail").click(projectClick);
-	
+}
 function projectClick(e) {
     // prevent the page from reloading   
     e.preventDefault();
